03ede1dfec7140af6af827d13b9cedc154534361
mmazlah <mmazlah@guidewire.com>
2024-01-16 23:27:15 +0800

    added initial commit  for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
+import cart_icon from "../assets/cart_icon.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+    const [menu, setMenu] = useState("shop")
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={()=>{setMenu("shop")}}>Shop {menu === "shop" ?<hr />:<></>} </li>
+        <li onClick={()=>{setMenu("men")}}>Men{menu === "men" ?<hr />:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women{menu === "women" ?<hr />:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids" ?<hr />:<></>}</li>
+        <div className="nav-login-cart">
+          <button>Login</button>
+          <img src={cart_icon} alt="" />
+          <div className="nav-cart-count">0</div>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
